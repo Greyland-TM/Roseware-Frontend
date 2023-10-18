af82b1d5afd651fd5f95e21a9e4633fb55550f8a
Greyland Miller <greyland1993@gmail.com>
2023-10-17 22:35:54 -0700

    updated next config to allow s3 uls

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["roseware-bucket.s3.amazonaws.com"],
+  },
+};
